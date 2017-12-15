85e294a137a4630a5574bcab4e496b2bab8060db
SimonLYU <7964852@qq.com>
2017-12-15 14:43:04 +0800

    删除注释

--- xsp/src/util.lua
+++ xsp/src/util.lua
@@ -42,10 +42,6 @@ end
 function util.pullMenuToBottom()
 	util.move(250,1500,250,600)
 	mSleep(1000)
---	util.move(130,1600,130,500)
---	mSleep(500)
---	util.move(130,1600,130,500)
---	mSleep(500)
 end
 
 function util.closeMenuIfNecessary()
